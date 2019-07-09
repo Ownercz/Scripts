84368cf391e82fc060eae2729f810ce825883bc2
Radim Lipovčan <radim@lipovcan.cz>
2019-07-09 21:25:15 +0200

    Update R710-IPMIStatic.sh

--- R710-IPMI-TEMP/R710-IPMIStatic.sh
+++ R710-IPMI-TEMP/R710-IPMIStatic.sh
@@ -12,11 +12,11 @@
 # IPMI SETTINGS:
 # Modify to suit your needs.
 # DEFAULT IP: 192.168.0.120
-IPMIHOST=10.0.100.20
+IPMIHOST=192.168.4.103
 IPMIUSER=root
 IPMIPW=calvin
 
-printf "Activating manual fan speeds! (2160 RPM)" | systemd-cat -t R710-IPMI-TEMP
-echo "Activating manual fan speeds! (2160 RPM)" | slacktee.sh -t "R710-IPMI-TEMP [$(hostname)]"
+#printf "Activating manual fan speeds! (2160 RPM)" | systemd-cat -t R710-IPMI-TEMP
+#echo "Activating manual fan speeds! (2160 RPM)" | slacktee.sh -t "R710-IPMI-TEMP [$(hostname)]"
 ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x01 0x00
 ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x02 0xff 0x09
